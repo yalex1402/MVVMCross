239a7ea23534dd405c23eb8299680c026ca4b735
Alexander GL <yagarcia1402@gmail.com>
2020-05-23 09:36:23 -0500

    Add Android Project

---
+++ TipCalculator.Android/Views/TipView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Platforms.Android.Views;
+using TipCalculator.Core.ViewModels;
+
+namespace TipCalculator.Android.Views
+{
+	[Activity(Label = "@string/app_name")]
+	public class TipView : MvxActivity<TipViewModel>
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.TipPage);
+		}
+	}
+}
