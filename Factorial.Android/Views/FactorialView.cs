c478de76b03bd0504c6b0ffc7dd03dae22369d97
Alexander GL <yagarcia1402@gmail.com>
2020-05-24 11:38:08 -0500

    Add FactorialViews in Android

---
+++ Factorial.Android/Views/FactorialView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Factorial.Core.ViewModels;
+using MvvmCross.Platforms.Android.Views;
+
+namespace Factorial.Android.Views
+{
+	[Activity(Label = "@string/app_name")]
+	public class FactorialView : MvxActivity<FactorialViewModel>
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.FactorialPage);
+		}
+	}
+}
---
+++ Factorial.Android/Views/SplashView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Factorial.Core;
+using MvvmCross.Platforms.Android.Core;
+using MvvmCross.Platforms.Android.Views;
+
+namespace Factorial.Android.Views
+{
+	[Activity(
+		Label = "@string/app_name",
+		MainLauncher = true,
+		Icon = "@drawable/icon",
+		Theme = "@style/Theme.Splash",
+		NoHistory = true,
+		ScreenOrientation = ScreenOrientation.Portrait)]
+	public class SplashView : MvxSplashScreenActivity<MvxAndroidSetup<App>, App>
+	{
+		public SplashView() : base(Resource.Layout.SplashPage)
+		{
+		}
+	}
+}
