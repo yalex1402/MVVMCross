f6e08f2893971b9b4cb9cf63a3870f9128a5f71a
Alexander GL <yagarcia1402@gmail.com>
2020-05-28 10:09:45 -0500

    Add Calculator.Android

---
+++ Calculator.Android/Views/CalculatorView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Calculator.Core.ViewModels;
+using MvvmCross.Platforms.Android.Views;
+
+namespace Calculator.Android.Views
+{
+	[Activity(Label = "@string/app_name")]
+	public class FactorialView : MvxActivity<CalculatorViewModel>
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.CalculatorPage);
+		}
+	}
+}
